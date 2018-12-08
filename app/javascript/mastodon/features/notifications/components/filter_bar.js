a204f8d33e0b7ddec513120d417c61821fd036a7
alxd <github@alxd.org>
2018-12-08 09:37:36 +0100

    add fancy onMouseOver for advanced view

--- app/javascript/mastodon/features/notifications/components/filter_bar.js
+++ app/javascript/mastodon/features/notifications/components/filter_bar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 
 const tooltips = defineMessages({
+  all: { id: 'notifications.filter.all', defaultMessage: 'All' },
   mentions: { id: 'notifications.filter.mentions', defaultMessage: 'Mentions' },
   favourites: { id: 'notifications.filter.favourites', defaultMessage: 'Favourites' },
   boosts: { id: 'notifications.filter.boosts', defaultMessage: 'Boosts' },
@@ -11,6 +12,13 @@ const tooltips = defineMessages({
 
 export default @injectIntl
 class FilterBar extends React.PureComponent {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      extendedElement: undefined,
+    };
+  };
 
   static propTypes = {
     selectFilter: PropTypes.func.isRequired,
@@ -23,8 +31,17 @@ class FilterBar extends React.PureComponent {
     return () => this.props.selectFilter(notificationType);
   }
 
+  changeExtendedElement (newElement) {
+    return () => {
+      this.setState({
+        extendedElement: newElement,
+      });
+    };
+  }
+
   render () {
     const { selectedFilter, advancedMode, intl } = this.props;
+    const { extendedElement } = this.state;
     const renderedElement = !advancedMode ? (
       <div className='notification__filter-bar'>
         <button
@@ -51,39 +68,92 @@ class FilterBar extends React.PureComponent {
         <button
           className={selectedFilter === 'all' ? 'active' : ''}
           onClick={this.onClick('all')}
+          title={intl.formatMessage(tooltips.all)}
+          onMouseOver={this.changeExtendedElement('all')}
+          onFocus={this.changeExtendedElement('all')}
+          onMouseLeave={this.changeExtendedElement(undefined)}
+          onBlur={this.changeExtendedElement(undefined)}
         >
-          <FormattedMessage
-            id='notifications.filter.all'
-            defaultMessage='All'
-          />
+          {extendedElement === 'all' ? (
+            <FormattedMessage
+              id='notifications.filter.all'
+              defaultMessage='All'
+            />
+          ) : (
+            <i className='fa fa-fw fa-bell' />
+          )}
         </button>
         <button
           className={selectedFilter === 'mention' ? 'active' : ''}
           onClick={this.onClick('mention')}
           title={intl.formatMessage(tooltips.mentions)}
+          onMouseOver={this.changeExtendedElement('mention')}
+          onFocus={this.changeExtendedElement('mention')}
+          onMouseLeave={this.changeExtendedElement(undefined)}
+          onBlur={this.changeExtendedElement(undefined)}
         >
-          <i className='fa fa-fw fa-at' />
+          {extendedElement === 'mention' ? (
+            <FormattedMessage
+              id='notifications.filter.mention'
+              defaultMessage='Mention'
+            />
+          ) : (
+            <i className='fa fa-fw fa-at' />
+          )}
         </button>
         <button
           className={selectedFilter === 'favourite' ? 'active' : ''}
           onClick={this.onClick('favourite')}
           title={intl.formatMessage(tooltips.favourites)}
+          onMouseOver={this.changeExtendedElement('favourite')}
+          onFocus={this.changeExtendedElement('favourite')}
+          onMouseLeave={this.changeExtendedElement(undefined)}
+          onBlur={this.changeExtendedElement(undefined)}
         >
-          <i className='fa fa-fw fa-star' />
+          {extendedElement === 'favourite' ? (
+            <FormattedMessage
+              id='notifications.filter.favourite'
+              defaultMessage='Favourites'
+            />
+          ) : (
+            <i className='fa fa-fw fa-star' />
+          )}
         </button>
         <button
           className={selectedFilter === 'reblog' ? 'active' : ''}
           onClick={this.onClick('reblog')}
           title={intl.formatMessage(tooltips.boosts)}
+          onMouseOver={this.changeExtendedElement('reblog')}
+          onFocus={this.changeExtendedElement('reblog')}
+          onMouseLeave={this.changeExtendedElement(undefined)}
+          onBlur={this.changeExtendedElement(undefined)}
         >
-          <i className='fa fa-fw fa-retweet' />
+          {extendedElement === 'reblog' ? (
+            <FormattedMessage
+              id='notifications.filter.reblog'
+              defaultMessage='Boost'
+            />
+          ) : (
+            <i className='fa fa-fw fa-retweet' />
+          )}
         </button>
         <button
           className={selectedFilter === 'follow' ? 'active' : ''}
           onClick={this.onClick('follow')}
           title={intl.formatMessage(tooltips.follows)}
+          onMouseOver={this.changeExtendedElement('follow')}
+          onFocus={this.changeExtendedElement('follow')}
+          onMouseLeave={this.changeExtendedElement(undefined)}
+          onBlur={this.changeExtendedElement(undefined)}
         >
-          <i className='fa fa-fw fa-user-plus' />
+          {extendedElement === 'follow' ? (
+            <FormattedMessage
+              id='notifications.filter.follow'
+              defaultMessage='Follows'
+            />
+          ) : (
+            <i className='fa fa-fw fa-user-plus' />
+          )}
         </button>
       </div>
     );
